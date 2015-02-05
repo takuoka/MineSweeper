98c39c8e5d55ff6ffd2aaff54b4de6817e3b6793
Takuya.Okamoto <blackn.red42@gmail.com>
2015-02-06 00:04:19 +0900

    gameManagerのモジュール化

--- build/gameManager.js
+++ build/gameManager.js
@@ -1,178 +1,184 @@
 (function() {
-  var BOMB_CHAR, BOMB_NUM, COVER_CHAR, FLAG_CHAR, FRONT_BOARD, SIZE_X, SIZE_Y, UNDER_BOARD, dumpBoard, getRandomPlace, getSorroundingPlace, initBoards, initFrontBoard, initUnderBoard, open, putFlag, rand, wait;
+  var game, generateGame, wait;
 
   console.log("gameManager.coffee");
 
-  SIZE_X = 5;
-
-  SIZE_Y = 4;
-
-  BOMB_NUM = 5;
-
-  UNDER_BOARD = [];
-
-  FRONT_BOARD = [];
-
-  BOMB_CHAR = 'B';
-
-  FLAG_CHAR = 'F';
-
-  COVER_CHAR = 'C';
-
-  initBoards = function(xSize, ySize, bomNum) {
-    console.log("initBoards");
-    initFrontBoard(xSize, ySize);
-    return initUnderBoard(xSize, ySize, bomNum);
-  };
-
-  initFrontBoard = function(xSize, ySize) {
-    var x, y, _i, _ref, _results;
-    _results = [];
-    for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
-      FRONT_BOARD.push([]);
-      _results.push((function() {
-        var _j, _ref1, _results1;
-        _results1 = [];
+  generateGame = function(sizeX, sizeY, bomNum) {
+    var API, BOMB_CHAR, BOMB_NUM, COVER_CHAR, FLAG_CHAR, FRONT_BOARD, SIZE_X, SIZE_Y, UNDER_BOARD, dumpBoard, dumpBoards, getRandomPlace, getSorroundingPlace, initBoards, initFrontBoard, initUnderBoard, open, putFlag, rand;
+    SIZE_X = 5;
+    SIZE_Y = 5;
+    BOMB_NUM = 5;
+    if (sizeX) {
+      SIZE_X = sizeX;
+    }
+    if (sizeY) {
+      SIZE_Y = sizeY;
+    }
+    if (bomNum) {
+      BOMB_NUM = bomNum;
+    }
+    UNDER_BOARD = [];
+    FRONT_BOARD = [];
+    BOMB_CHAR = 'B';
+    FLAG_CHAR = 'F';
+    COVER_CHAR = 'C';
+    initBoards = function(xSize, ySize, bomNum) {
+      console.log("initBoards");
+      initFrontBoard(xSize, ySize);
+      return initUnderBoard(xSize, ySize, bomNum);
+    };
+    initFrontBoard = function(xSize, ySize) {
+      var x, y, _i, _ref, _results;
+      _results = [];
+      for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
+        FRONT_BOARD.push([]);
+        _results.push((function() {
+          var _j, _ref1, _results1;
+          _results1 = [];
+          for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
+            _results1.push(FRONT_BOARD[y].push(COVER_CHAR));
+          }
+          return _results1;
+        })());
+      }
+      return _results;
+    };
+    initUnderBoard = function(xSize, ySize, bomNum) {
+      var addBomb, i, incrementSorroundingNumbers, x, y, _i, _j, _k, _l, _ref, _ref1, _ref2, _ref3, _results;
+      addBomb = function() {
+        var p;
+        p = getRandomPlace(xSize, ySize);
+        if (UNDER_BOARD[p.y][p.x] !== BOMB_CHAR) {
+          return UNDER_BOARD[p.y][p.x] = BOMB_CHAR;
+        } else {
+          return addBomb();
+        }
+      };
+      incrementSorroundingNumbers = function(x, y) {
+        return getSorroundingPlace(x, y, function(sx, sy) {
+          if (UNDER_BOARD[sy][sx] !== BOMB_CHAR) {
+            return UNDER_BOARD[sy][sx] += 1;
+          }
+        });
+      };
+      for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
+        UNDER_BOARD.push([]);
         for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
-          _results1.push(FRONT_BOARD[y].push(COVER_CHAR));
+          UNDER_BOARD[y].push(0);
         }
-        return _results1;
-      })());
-    }
-    return _results;
-  };
-
-  initUnderBoard = function(xSize, ySize, bomNum) {
-    var addBomb, i, incrementSorroundingNumbers, x, y, _i, _j, _k, _l, _ref, _ref1, _ref2, _ref3, _results;
-    addBomb = function() {
-      var p;
-      p = getRandomPlace(xSize, ySize);
-      if (UNDER_BOARD[p.y][p.x] !== BOMB_CHAR) {
-        return UNDER_BOARD[p.y][p.x] = BOMB_CHAR;
-      } else {
-        return addBomb();
+      }
+      for (i = _k = 0, _ref2 = bomNum - 1; 0 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 0 <= _ref2 ? ++_k : --_k) {
+        addBomb();
+      }
+      _results = [];
+      for (y = _l = 0, _ref3 = ySize - 1; 0 <= _ref3 ? _l <= _ref3 : _l >= _ref3; y = 0 <= _ref3 ? ++_l : --_l) {
+        _results.push((function() {
+          var _m, _ref4, _results1;
+          _results1 = [];
+          for (x = _m = 0, _ref4 = xSize - 1; 0 <= _ref4 ? _m <= _ref4 : _m >= _ref4; x = 0 <= _ref4 ? ++_m : --_m) {
+            if (UNDER_BOARD[y][x] === BOMB_CHAR) {
+              _results1.push(incrementSorroundingNumbers(x, y));
+            } else {
+              _results1.push(void 0);
+            }
+          }
+          return _results1;
+        })());
+      }
+      return _results;
+    };
+    open = function(x, y) {
+      var value;
+      value = UNDER_BOARD[y][x];
+      FRONT_BOARD[y][x] = value;
+      if (value === 0) {
+        getSorroundingPlace(x, y, function(sx, sy) {
+          if (FRONT_BOARD[sy][sx] === COVER_CHAR) {
+            return open(sx, sy);
+          }
+        });
+      }
+      return value;
+    };
+    putFlag = function(x, y) {
+      if (FRONT_BOARD[y][x] === COVER_CHAR) {
+        return FRONT_BOARD[y][x] = FLAG_CHAR;
+      } else if (FRONT_BOARD[y][x] === FLAG_CHAR) {
+        return FRONT_BOARD[y][x] = COVER_CHAR;
       }
     };
-    incrementSorroundingNumbers = function(x, y) {
-      return getSorroundingPlace(x, y, function(sx, sy) {
-        if (UNDER_BOARD[sy][sx] !== BOMB_CHAR) {
-          return UNDER_BOARD[sy][sx] += 1;
-        }
-      });
+    rand = function(max) {
+      return Math.round(Math.random() * max);
     };
-    for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
-      UNDER_BOARD.push([]);
-      for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
-        UNDER_BOARD[y].push(0);
-      }
-    }
-    for (i = _k = 0, _ref2 = bomNum - 1; 0 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 0 <= _ref2 ? ++_k : --_k) {
-      addBomb();
-    }
-    _results = [];
-    for (y = _l = 0, _ref3 = ySize - 1; 0 <= _ref3 ? _l <= _ref3 : _l >= _ref3; y = 0 <= _ref3 ? ++_l : --_l) {
-      _results.push((function() {
-        var _m, _ref4, _results1;
-        _results1 = [];
-        for (x = _m = 0, _ref4 = xSize - 1; 0 <= _ref4 ? _m <= _ref4 : _m >= _ref4; x = 0 <= _ref4 ? ++_m : --_m) {
-          if (UNDER_BOARD[y][x] === BOMB_CHAR) {
-            _results1.push(incrementSorroundingNumbers(x, y));
-          } else {
-            _results1.push(void 0);
+    getRandomPlace = function(xSize, ySize) {
+      var place;
+      place = {};
+      place.x = rand(xSize - 1);
+      place.y = rand(ySize - 1);
+      return place;
+    };
+    getSorroundingPlace = function(x, y, callback) {
+      var checkAndDo;
+      checkAndDo = function(x, y) {
+        if (UNDER_BOARD[y] !== void 0) {
+          if (UNDER_BOARD[y][x] !== void 0) {
+            return callback(x, y);
           }
         }
-        return _results1;
-      })());
-    }
-    return _results;
-  };
-
-  open = function(x, y) {
-    var value;
-    value = UNDER_BOARD[y][x];
-    FRONT_BOARD[y][x] = value;
-    if (value === 0) {
-      getSorroundingPlace(x, y, function(sx, sy) {
-        if (FRONT_BOARD[sy][sx] === COVER_CHAR) {
-          return open(sx, sy);
-        }
-      });
-    }
-    return value;
-  };
-
-  putFlag = function(x, y) {
-    if (FRONT_BOARD[y][x] === COVER_CHAR) {
-      return FRONT_BOARD[y][x] = FLAG_CHAR;
-    } else if (FRONT_BOARD[y][x] === FLAG_CHAR) {
-      return FRONT_BOARD[y][x] = COVER_CHAR;
-    }
-  };
-
-  rand = function(max) {
-    return Math.round(Math.random() * max);
-  };
-
-  getRandomPlace = function(xSize, ySize) {
-    var place;
-    place = {};
-    place.x = rand(xSize - 1);
-    place.y = rand(ySize - 1);
-    return place;
-  };
-
-  getSorroundingPlace = function(x, y, callback) {
-    var checkAndDo;
-    checkAndDo = function(x, y) {
-      if (UNDER_BOARD[y] !== void 0) {
-        if (UNDER_BOARD[y][x] !== void 0) {
-          return callback(x, y);
-        }
-      }
+      };
+      checkAndDo(x - 1, y - 1);
+      checkAndDo(x - 1, y);
+      checkAndDo(x - 1, y + 1);
+      checkAndDo(x, y - 1);
+      checkAndDo(x, y + 1);
+      checkAndDo(x + 1, y - 1);
+      checkAndDo(x + 1, y);
+      return checkAndDo(x + 1, y + 1);
     };
-    checkAndDo(x - 1, y - 1);
-    checkAndDo(x - 1, y);
-    checkAndDo(x - 1, y + 1);
-    checkAndDo(x, y - 1);
-    checkAndDo(x, y + 1);
-    checkAndDo(x + 1, y - 1);
-    checkAndDo(x + 1, y);
-    return checkAndDo(x + 1, y + 1);
-  };
-
-  dumpBoard = function(board) {
-    var str, x, xSize, y, ySize, _i, _j, _ref, _ref1;
-    console.log("---------");
-    ySize = board.length;
-    xSize = board[0].length;
-    for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
-      str = "";
-      for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
-        str += board[y][x] + " ";
+    dumpBoard = function(board) {
+      var str, x, xSize, y, ySize, _i, _j, _ref, _ref1;
+      console.log("---------");
+      ySize = board.length;
+      xSize = board[0].length;
+      for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
+        str = "";
+        for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
+          str += board[y][x] + " ";
+        }
+        console.log(str);
       }
-      console.log(str);
-    }
-    return console.log("---------");
+      return console.log("---------");
+    };
+    dumpBoards = function() {
+      dumpBoard(UNDER_BOARD);
+      dumpBoard(FRONT_BOARD);
+      return console.log("==============");
+    };
+    initBoards(SIZE_X, SIZE_Y, BOMB_NUM);
+    dumpBoard(UNDER_BOARD);
+    API = {};
+    API.open = open;
+    API.putFlag = putFlag;
+    API.dumpBoards = dumpBoards;
+    return API;
   };
 
-  initBoards(SIZE_X, SIZE_Y, 5);
+  game = generateGame(10, 10, 50);
 
-  dumpBoard(UNDER_BOARD);
+  game.open(0, 0);
 
-  open(0, 0);
-
-  dumpBoard(FRONT_BOARD);
+  game.dumpBoards();
 
   wait = function(time, callback) {
     return setTimeout(callback, time);
   };
 
   wait(1000, function() {
-    putFlag(2, 2);
-    dumpBoard(FRONT_BOARD);
+    game.putFlag(2, 2);
+    game.dumpBoards();
     return wait(1000, function() {
-      putFlag(2, 2);
-      return dumpBoard(FRONT_BOARD);
+      game.putFlag(2, 2);
+      return game.dumpBoards();
     });
   });
 
--- src/gameManager.coffee
+++ src/gameManager.coffee
@@ -1,139 +1,162 @@
 console.log "gameManager.coffee"
 
-#盤面のサイズ
-SIZE_X = 5
-SIZE_Y = 4
-
-#地雷の数
-BOMB_NUM = 5
-
-#盤面
-UNDER_BOARD = []#盤面の中身
-FRONT_BOARD = []#ユーザーに見えてる盤面
-
-#配列上での記号の定義
-BOMB_CHAR = 'B'#地雷
-FLAG_CHAR = 'F'#地雷があると確信したときに置くマーク
-COVER_CHAR = 'C'#開いていない状態
+generateGame = (sizeX, sizeY, bomNum) ->
 
 
-initBoards =  (xSize, ySize, bomNum)->
-	console.log "initBoards"
-	initFrontBoard(xSize, ySize)
-	initUnderBoard(xSize, ySize, bomNum)
+	#盤面のサイズ, 地雷の数
+	SIZE_X   = 5
+	SIZE_Y   = 5
+	BOMB_NUM = 5
+	#引数があれば代入
+	SIZE_X   = sizeX if sizeX
+	SIZE_Y   = sizeY if sizeY
+	BOMB_NUM = bomNum if bomNum
 
 
-initFrontBoard = (xSize, ySize) ->
-	for y in [0..ySize-1]
-		FRONT_BOARD.push []
-		for x in [0..xSize-1]
-			FRONT_BOARD[y].push COVER_CHAR
+
+	#盤面
+	UNDER_BOARD = []#盤面の中身
+	FRONT_BOARD = []#ユーザーに見えてる盤面
+
+	#配列上での記号の定義
+	BOMB_CHAR  = 'B'#地雷
+	FLAG_CHAR  = 'F'#地雷があると確信したときに置くマーク
+	COVER_CHAR = 'C'#開いていない状態
 
 
-initUnderBoard = (xSize, ySize, bomNum) ->
-	addBomb = ->
-		p = getRandomPlace xSize, ySize
-		if UNDER_BOARD[p.y][p.x] isnt BOMB_CHAR
-			UNDER_BOARD[p.y][p.x] = BOMB_CHAR
-		else
+	initBoards =  (xSize, ySize, bomNum)->
+		console.log "initBoards"
+		initFrontBoard(xSize, ySize)
+		initUnderBoard(xSize, ySize, bomNum)
+
+
+	initFrontBoard = (xSize, ySize) ->
+		for y in [0..ySize-1]
+			FRONT_BOARD.push []
+			for x in [0..xSize-1]
+				FRONT_BOARD[y].push COVER_CHAR
+
+
+	initUnderBoard = (xSize, ySize, bomNum) ->
+		addBomb = ->
+			p = getRandomPlace xSize, ySize
+			if UNDER_BOARD[p.y][p.x] isnt BOMB_CHAR
+				UNDER_BOARD[p.y][p.x] = BOMB_CHAR
+			else
+				addBomb()
+		incrementSorroundingNumbers = (x, y) ->
+			getSorroundingPlace x, y, (sx, sy)->
+				if UNDER_BOARD[sy][sx] isnt BOMB_CHAR
+					UNDER_BOARD[sy][sx] += 1
+
+		#２つの盤面の初期化
+		for y in [0..ySize-1]
+			UNDER_BOARD.push []
+			for x in [0..xSize-1]
+				UNDER_BOARD[y].push 0
+
+		#地雷の追加
+		for i in [0..bomNum-1]
 			addBomb()
-	incrementSorroundingNumbers = (x, y) ->
-		getSorroundingPlace x, y, (sx, sy)->
-			if UNDER_BOARD[sy][sx] isnt BOMB_CHAR
-				UNDER_BOARD[sy][sx] += 1
 
-	#２つの盤面の初期化
-	for y in [0..ySize-1]
-		UNDER_BOARD.push []
-		for x in [0..xSize-1]
-			UNDER_BOARD[y].push 0
-
-	#地雷の追加
-	for i in [0..bomNum-1]
-		addBomb()
-
-	#番号の追加
-	for y in [0..ySize-1]
-		for x in [0..xSize-1]
-			if UNDER_BOARD[y][x] is BOMB_CHAR
-				incrementSorroundingNumbers x, y
+		#番号の追加
+		for y in [0..ySize-1]
+			for x in [0..xSize-1]
+				if UNDER_BOARD[y][x] is BOMB_CHAR
+					incrementSorroundingNumbers x, y
 
 
-open = (x, y) ->
-	value = UNDER_BOARD[y][x]
-	FRONT_BOARD[y][x] = value
-	# もし 0 なら 周りのマスも開ける
-	if value is 0
-		getSorroundingPlace x, y, (sx, sy)->
-			if FRONT_BOARD[sy][sx] is COVER_CHAR
-				open sx, sy
-	return value
+	open = (x, y) ->
+		value = UNDER_BOARD[y][x]
+		FRONT_BOARD[y][x] = value
+		# もし 0 なら 周りのマスも開ける
+		if value is 0
+			getSorroundingPlace x, y, (sx, sy)->
+				if FRONT_BOARD[sy][sx] is COVER_CHAR
+					open sx, sy
+		return value
 
 
-putFlag = (x, y) ->
-	if FRONT_BOARD[y][x] is COVER_CHAR
-		FRONT_BOARD[y][x] = FLAG_CHAR
-	else if FRONT_BOARD[y][x] is FLAG_CHAR
-		FRONT_BOARD[y][x] = COVER_CHAR
+	putFlag = (x, y) ->
+		if FRONT_BOARD[y][x] is COVER_CHAR
+			FRONT_BOARD[y][x] = FLAG_CHAR
+		else if FRONT_BOARD[y][x] is FLAG_CHAR
+			FRONT_BOARD[y][x] = COVER_CHAR
 
 
 
-# --------------- util methods ------------------
+	# --------------- util methods ------------------
 
-rand = (max) ->
-	return Math.round Math.random() * max
-getRandomPlace = (xSize, ySize)->
-	place = {}
-	place.x = rand xSize - 1
-	place.y = rand ySize - 1
-	return place
-
-
-getSorroundingPlace = (x, y, callback) ->
-	checkAndDo = (x, y) ->
-		if UNDER_BOARD[y] isnt undefined
-			if UNDER_BOARD[y][x] isnt undefined
-				callback(x, y)
-	checkAndDo x-1, y-1
-	checkAndDo x-1, y
-	checkAndDo x-1, y+1
-	checkAndDo x  , y-1
-	checkAndDo x  , y+1
-	checkAndDo x+1, y-1
-	checkAndDo x+1, y
-	checkAndDo x+1, y+1
+	rand = (max) ->
+		return Math.round Math.random() * max
+	getRandomPlace = (xSize, ySize)->
+		place = {}
+		place.x = rand xSize - 1
+		place.y = rand ySize - 1
+		return place
 
 
-
-dumpBoard = (board)->
-	console.log "---------"
-	ySize = board.length
-	xSize = board[0].length
-	for y in [0..ySize-1]
-		str = ""
-		for x in [0..xSize-1]
-			str += board[y][x] + " "
-		console.log str
-	console.log "---------"
+	getSorroundingPlace = (x, y, callback) ->
+		checkAndDo = (x, y) ->
+			if UNDER_BOARD[y] isnt undefined
+				if UNDER_BOARD[y][x] isnt undefined
+					callback(x, y)
+		checkAndDo x-1, y-1
+		checkAndDo x-1, y
+		checkAndDo x-1, y+1
+		checkAndDo x  , y-1
+		checkAndDo x  , y+1
+		checkAndDo x+1, y-1
+		checkAndDo x+1, y
+		checkAndDo x+1, y+1
 
 
 
-initBoards SIZE_X, SIZE_Y, 5
-dumpBoard UNDER_BOARD
+	dumpBoard = (board)->
+		console.log "---------"
+		ySize = board.length
+		xSize = board[0].length
+		for y in [0..ySize-1]
+			str = ""
+			for x in [0..xSize-1]
+				str += board[y][x] + " "
+			console.log str
+		console.log "---------"
 
 
-open 0,0
-dumpBoard FRONT_BOARD
+	dumpBoards = ->
+		dumpBoard UNDER_BOARD
+		dumpBoard FRONT_BOARD
+		console.log "=============="
+
+
+	initBoards SIZE_X, SIZE_Y, BOMB_NUM
+	dumpBoard UNDER_BOARD
+
+	API = {}
+	API.open = open
+	API.putFlag = putFlag
+	API.dumpBoards = dumpBoards
+	return API
+
+
+
+game = generateGame 10, 10, 50
+
+
+
+game.open 0,0
+game.dumpBoards()
 
 wait = (time, callback)-> setTimeout callback, time
 
 wait 1000, ->
-	putFlag 2,2
-	dumpBoard FRONT_BOARD
+	game.putFlag 2,2
+	game.dumpBoards()
 
 	wait 1000, ->
-		putFlag 2,2
-		dumpBoard FRONT_BOARD
+		game.putFlag 2,2
+		game.dumpBoards()
 
 
 
