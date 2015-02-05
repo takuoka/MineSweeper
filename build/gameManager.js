ee58fe54de6293d8c00aa1acba036230a9b170ec
Takuya.Okamoto <blackn.red42@gmail.com>
2015-02-05 21:57:14 +0900

    盤面の生成

--- build/gameManager.js
+++ build/gameManager.js
@@ -1,4 +1,127 @@
 (function() {
+  var BOMB_CHAR, FLAG_CHAR, QUESTION_CHAR, contentsBoard, dumpBoard, horizonalSize, initContentsBoard, initUserBoard, numberOfBomb, userBoard, verticalSize;
+
   console.log("gameManager.coffee");
+
+  verticalSize = 5;
+
+  horizonalSize = 6;
+
+  numberOfBomb = 5;
+
+  userBoard = [];
+
+  contentsBoard = [];
+
+  BOMB_CHAR = 'B';
+
+  QUESTION_CHAR = '?';
+
+  FLAG_CHAR = 'F';
+
+  initUserBoard = function(vSize, hSize) {
+    var h, v, _i, _ref, _results;
+    _results = [];
+    for (v = _i = 0, _ref = vSize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; v = 0 <= _ref ? ++_i : --_i) {
+      userBoard.push([]);
+      _results.push((function() {
+        var _j, _ref1, _results1;
+        _results1 = [];
+        for (h = _j = 0, _ref1 = hSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; h = 0 <= _ref1 ? ++_j : --_j) {
+          _results1.push(userBoard[v].push(''));
+        }
+        return _results1;
+      })());
+    }
+    return _results;
+  };
+
+  initContentsBoard = function(vSize, hSize, bomNum) {
+    var addBomb, getRandomPlace, h, i, incrementSorroundingNumbers, rand, v, _i, _j, _k, _l, _ref, _ref1, _ref2, _ref3, _results;
+    console.log("initContentsBoard");
+    for (v = _i = 0, _ref = vSize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; v = 0 <= _ref ? ++_i : --_i) {
+      contentsBoard.push([]);
+      for (h = _j = 0, _ref1 = hSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; h = 0 <= _ref1 ? ++_j : --_j) {
+        contentsBoard[v].push(0);
+      }
+    }
+    rand = function(max) {
+      return Math.round(Math.random() * max);
+    };
+    getRandomPlace = function() {
+      var place;
+      place = {};
+      place.v = rand(vSize - 1);
+      place.h = rand(hSize - 1);
+      return place;
+    };
+    addBomb = function() {
+      var p;
+      p = getRandomPlace();
+      if (contentsBoard[p.v][p.h] !== BOMB_CHAR) {
+        return contentsBoard[p.v][p.h] = BOMB_CHAR;
+      } else {
+        return addBomb();
+      }
+    };
+    incrementSorroundingNumbers = function(v, h) {
+      var increment;
+      increment = function(v, h) {
+        if (contentsBoard[v] !== void 0) {
+          if (contentsBoard[v][h] !== void 0) {
+            if (contentsBoard[v][h] !== BOMB_CHAR) {
+              return contentsBoard[v][h] += 1;
+            }
+          }
+        }
+      };
+      increment(v - 1, h - 1);
+      increment(v - 1, h);
+      increment(v - 1, h + 1);
+      increment(v, h - 1);
+      increment(v, h + 1);
+      increment(v + 1, h - 1);
+      increment(v + 1, h);
+      return increment(v + 1, h + 1);
+    };
+    for (i = _k = 0, _ref2 = bomNum - 1; 0 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 0 <= _ref2 ? ++_k : --_k) {
+      addBomb();
+    }
+    _results = [];
+    for (v = _l = 0, _ref3 = vSize - 1; 0 <= _ref3 ? _l <= _ref3 : _l >= _ref3; v = 0 <= _ref3 ? ++_l : --_l) {
+      _results.push((function() {
+        var _m, _ref4, _results1;
+        _results1 = [];
+        for (h = _m = 0, _ref4 = hSize - 1; 0 <= _ref4 ? _m <= _ref4 : _m >= _ref4; h = 0 <= _ref4 ? ++_m : --_m) {
+          if (contentsBoard[v][h] === BOMB_CHAR) {
+            _results1.push(incrementSorroundingNumbers(v, h));
+          } else {
+            _results1.push(void 0);
+          }
+        }
+        return _results1;
+      })());
+    }
+    return _results;
+  };
+
+  dumpBoard = function(board) {
+    var h, hSize, str, v, vSize, _i, _j, _ref, _ref1, _results;
+    vSize = board.length;
+    hSize = board[0].length;
+    _results = [];
+    for (v = _i = 0, _ref = vSize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; v = 0 <= _ref ? ++_i : --_i) {
+      str = "";
+      for (h = _j = 0, _ref1 = hSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; h = 0 <= _ref1 ? ++_j : --_j) {
+        str += contentsBoard[v][h] + " ";
+      }
+      _results.push(console.log(str));
+    }
+    return _results;
+  };
+
+  initContentsBoard(verticalSize, horizonalSize, 5);
+
+  dumpBoard(contentsBoard);
 
 }).call(this);
--- src/gameManager.coffee
+++ src/gameManager.coffee
@@ -1,1 +1,108 @@
 console.log "gameManager.coffee"
+
+#盤面のサイズ
+verticalSize = 5
+horizonalSize = 6
+
+#地雷の数
+numberOfBomb = 5
+
+#盤面
+userBoard = []#ユーザーに見えてる盤面
+contentsBoard = []#盤面の中身
+
+
+#配列上での記号の定義
+BOMB_CHAR = 'B'
+QUESTION_CHAR = '?'#地雷があるかもしれないときに置くマーク
+FLAG_CHAR = 'F'#地雷があると確信したときに置くマーク
+
+
+
+initUserBoard = (vSize, hSize) ->
+	for v in [0..vSize-1]
+		userBoard.push []
+		for h in [0..hSize-1]
+			userBoard[v].push ''
+
+
+
+initContentsBoard = (vSize, hSize, bomNum) ->
+	console.log "initContentsBoard"
+	for v in [0..vSize-1]
+		contentsBoard.push []
+		for h in [0..hSize-1]
+			contentsBoard[v].push 0
+
+	rand = (max) -> Math.round Math.random() * max
+
+	getRandomPlace = ->
+		place = {}
+		place.v = rand vSize - 1
+		place.h = rand hSize - 1
+		return place
+
+	addBomb = ->
+		p = getRandomPlace()
+		if contentsBoard[p.v][p.h] isnt BOMB_CHAR
+			contentsBoard[p.v][p.h] = BOMB_CHAR
+		else
+			addBomb()
+
+	incrementSorroundingNumbers = (v, h) ->
+		increment = (v, h) ->
+			if contentsBoard[v] isnt undefined
+				if contentsBoard[v][h] isnt undefined
+					if contentsBoard[v][h] isnt BOMB_CHAR
+						contentsBoard[v][h] += 1
+		increment v-1, h-1
+		increment v-1, h
+		increment v-1, h+1
+		increment v, h-1
+		increment v, h+1
+		increment v+1, h-1
+		increment v+1, h
+		increment v+1, h+1
+
+
+	for i in [0..bomNum-1]
+		addBomb()
+
+	for v in [0..vSize-1]
+		for h in [0..hSize-1]
+			if contentsBoard[v][h] is BOMB_CHAR
+				incrementSorroundingNumbers v, h
+
+
+
+
+dumpBoard = (board)->
+	vSize = board.length
+	hSize = board[0].length
+
+	for v in [0..vSize-1]
+		str = ""
+		for h in [0..hSize-1]
+			str += contentsBoard[v][h] + " "
+		console.log str
+
+
+
+initContentsBoard verticalSize, horizonalSize, 5
+dumpBoard contentsBoard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
