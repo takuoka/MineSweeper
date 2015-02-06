0bcb34c4457e1bd8565248dac97e40ee11c8fdde
Takuya.Okamoto <blackn.red42@gmail.com>
2015-02-06 17:13:00 +0900

    動的にtableタグを作成する関数の実装

--- build/script.js
+++ build/script.js
@@ -1,8 +1,30 @@
 (function() {
-  var generateTable;
+  var createTable;
 
   console.log("script.coffee");
 
-  generateTable = function(id) {};
+  window.onload = function() {
+    return createTable("game", 4, 4);
+  };
+
+  createTable = function(id, xSize, ySize) {
+    var cell, div, gameDiv, row, table, tbody, x, y, _i, _j, _ref, _ref1;
+    gameDiv = document.getElementById(id);
+    table = document.createElement("table");
+    tbody = document.createElement("tbody");
+    for (y = _i = 0, _ref = ySize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; y = 0 <= _ref ? ++_i : --_i) {
+      row = document.createElement("tr");
+      for (x = _j = 0, _ref1 = xSize - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; x = 0 <= _ref1 ? ++_j : --_j) {
+        cell = document.createElement("td");
+        div = document.createElement("div");
+        div.innerHTML = (y * x) + x;
+        cell.appendChild(div);
+        row.appendChild(cell);
+      }
+      tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    return gameDiv.appendChild(table);
+  };
 
 }).call(this);
--- src/script.coffee
+++ src/script.coffee
@@ -1,8 +1,34 @@
 console.log "script.coffee"
 
 
-generateTable = (id)->
-	
+window.onload = ->
+	createTable "game", 4, 4
+
+
+
+
+createTable = (id, xSize, ySize)->
+
+	gameDiv = document.getElementById id
+	table   = document.createElement "table"
+	tbody   = document.createElement "tbody"
+
+	for y in [0..ySize-1]
+		row = document.createElement "tr"
+
+		for x in [0..xSize-1]
+			cell = document.createElement "td"
+			div  = document.createElement "div"
+			div.innerHTML = (y * x) + x
+			cell.appendChild div
+			row.appendChild cell
+
+		tbody.appendChild row
+
+	table.appendChild tbody
+	gameDiv.appendChild table
+
+
 
 
 # game = generateGame 10, 10, 30
