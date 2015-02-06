731d472cf7dd25348cf1bff709019cf02352ee62
Takuya.Okamoto <blackn.red42@gmail.com>
2015-02-07 03:24:29 +0900

    スタートボタンクリックでスタートスクリーンをフェードアウト

--- build/main.js
+++ build/main.js
@@ -1,10 +1,16 @@
 (function() {
-  var onGameClear, onGameOver;
+  var hide, onGameClear, onGameOver, wait;
 
   console.log("main.coffee");
 
-  window.onload = function() {
-    return generateGame("game", 7, 6, 3, onGameOver, onGameClear);
+  window.onClickStart = function() {
+    var bombNum, startScreen, xSize, ySize;
+    startScreen = document.getElementById("startScreen");
+    xSize = document.getElementById("xSize").value;
+    ySize = document.getElementById("ySize").value;
+    bombNum = document.getElementById("bombNum").value;
+    generateGame("game", xSize, ySize, bombNum, onGameOver, onGameClear);
+    return hide(startScreen);
   };
 
   onGameOver = function() {
@@ -13,6 +19,17 @@
 
   onGameClear = function() {
     return alert("cleared!!");
+  };
+
+  hide = function(elm) {
+    elm.style.opacity = 0;
+    return wait(500, function() {
+      return elm.style.display = "none";
+    });
+  };
+
+  wait = function(time, callback) {
+    return setTimeout(callback, time);
   };
 
 }).call(this);
--- src/main.coffee
+++ src/main.coffee
@@ -1,10 +1,28 @@
 console.log "main.coffee"
 
-window.onload = ->
-	generateGame "game", 7, 6, 3, onGameOver, onGameClear
+window.onClickStart =->
+
+	startScreen = document.getElementById "startScreen"
+	xSize       = document.getElementById("xSize").value
+	ySize       = document.getElementById("ySize").value
+	bombNum     = document.getElementById("bombNum").value
+
+	generateGame "game", xSize, ySize, bombNum, onGameOver, onGameClear
+
+	hide startScreen
+
 
 onGameOver = ->
 	alert "gameOver!!"
 
 onGameClear = ->
 	alert "cleared!!"
+
+
+hide = (elm)->
+	elm.style.opacity = 0
+	wait 500, -> elm.style.display = "none"
+
+
+
+wait = (time, callback)-> setTimeout callback, time
