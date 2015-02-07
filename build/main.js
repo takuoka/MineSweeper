10a7982d45fea12a57a9e6af1d8a082f6c82bd6c
Takuya.Okamoto <blackn.red42@gmail.com>
2015-02-07 19:37:08 +0900

    タイマーの実装

--- build/main.js
+++ build/main.js
@@ -1,11 +1,26 @@
 (function() {
-  var hide, onGameClear, onGameOver, wait;
+  var gameStart, getElapsedTime, hide, onGameClear, onGameOver, startTimer, stopTimer, tickInterval, wait, zeroPadding_2;
 
   console.log("main.coffee");
 
-  window.onload = function() {};
+  window.onload = function() {
+    return onClickStart();
+  };
 
   window.onClickStart = function() {
+    return gameStart();
+  };
+
+  onGameOver = function() {
+    alert("gameOver!!");
+    return stopTimer();
+  };
+
+  onGameClear = function() {
+    return alert("cleared!!");
+  };
+
+  gameStart = function() {
     var bombInfoNum, bombNum, startScreen, xSize, ySize;
     startScreen = document.getElementById("startScreen");
     xSize = document.getElementById("xSize").value;
@@ -13,17 +28,47 @@
     bombNum = document.getElementById("bombNum").value;
     bombInfoNum = document.getElementById("bombInfoNum");
     generateGame("game", xSize, ySize, bombNum, onGameOver, onGameClear);
-    console.log(bombInfoNum);
     bombInfoNum.innerHTML = bombNum;
-    return hide(startScreen);
+    hide(startScreen);
+    return startTimer();
   };
 
-  onGameOver = function() {
-    return alert("gameOver!!");
+  tickInterval = null;
+
+  stopTimer = function() {
+    return clearInterval(tickInterval);
   };
 
-  onGameClear = function() {
-    return alert("cleared!!");
+  startTimer = function() {
+    var startDate, tick, timerElm;
+    timerElm = document.getElementById("timer");
+    startDate = new Date();
+    tick = function() {
+      var t, timerString;
+      t = getElapsedTime(startDate);
+      timerString = t.minute + ":" + t.sec;
+      return timerElm.innerHTML = timerString;
+    };
+    return tickInterval = setInterval(tick, 1000);
+  };
+
+  getElapsedTime = function(startDate) {
+    var elapsedTime, hour, minute, nowDate, sec, totalTime;
+    nowDate = new Date();
+    totalTime = nowDate.getTime() - startDate.getTime();
+    hour = Math.floor(totalTime / (60 * 60 * 1000));
+    totalTime = totalTime - (hour * 60 * 60 * 1000);
+    minute = Math.floor(totalTime / (60 * 1000));
+    totalTime = totalTime - (minute * 60 * 1000);
+    sec = Math.floor(totalTime / 1000);
+    elapsedTime = {};
+    elapsedTime.minute = zeroPadding_2(minute);
+    elapsedTime.sec = zeroPadding_2(sec);
+    return elapsedTime;
+  };
+
+  zeroPadding_2 = function(num) {
+    return ("0" + num).slice(-2);
   };
 
   hide = function(elm) {
--- src/main.coffee
+++ src/main.coffee
@@ -1,11 +1,26 @@
 console.log "main.coffee"
 
+
+# -------------- events -----------------
+
 window.onload = ->
-	# onClickStart()
+	onClickStart()
+
+window.onClickStart = ->
+	gameStart()
+
+onGameOver = ->
+	alert "gameOver!!"
+	stopTimer()
+
+onGameClear = ->
+	alert "cleared!!"
 
 
-window.onClickStart =->
+# -------------- main function -----------------
 
+
+gameStart = ->
 	startScreen = document.getElementById "startScreen"
 	xSize       = document.getElementById("xSize").value
 	ySize       = document.getElementById("ySize").value
@@ -13,18 +28,54 @@ window.onClickStart =->
 	bombInfoNum = document.getElementById "bombInfoNum"
 
 	generateGame "game", xSize, ySize, bombNum, onGameOver, onGameClear
-
-	console.log bombInfoNum
 	bombInfoNum.innerHTML = bombNum
 
 	hide startScreen
 
+	startTimer()
 
-onGameOver = ->
-	alert "gameOver!!"
 
-onGameClear = ->
-	alert "cleared!!"
+
+
+# ---------------- timer -------------------------
+tickInterval = null;
+
+stopTimer = ->
+	clearInterval tickInterval
+
+startTimer = ->
+	timerElm = document.getElementById "timer"
+	startDate = new Date()
+
+	tick = ->
+		t = getElapsedTime startDate
+		timerString = t.minute + ":" + t.sec
+		timerElm.innerHTML = timerString
+	tickInterval = setInterval tick, 1000
+
+
+
+# ---------------- util function -------------------------
+
+getElapsedTime = (startDate)->
+	nowDate = new Date()
+	totalTime = nowDate.getTime() - startDate.getTime()
+
+	hour = Math.floor(totalTime/(60*60*1000))
+	totalTime = totalTime-(hour*60*60*1000)
+
+	minute = Math.floor(totalTime/(60*1000))
+	totalTime = totalTime-(minute*60*1000)
+
+	sec = Math.floor(totalTime/1000)
+
+	elapsedTime = {}
+	elapsedTime.minute = zeroPadding_2 minute
+	elapsedTime.sec = zeroPadding_2 sec
+	return elapsedTime
+
+
+zeroPadding_2 = (num) -> return ("0" + num).slice(-2)
 
 
 hide = (elm)->
@@ -32,5 +83,9 @@ hide = (elm)->
 	wait 500, -> elm.style.display = "none"
 
 
-
 wait = (time, callback)-> setTimeout callback, time
+
+
+
+
+
